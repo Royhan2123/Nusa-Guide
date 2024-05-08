1922600286a5dbd63d7cb208c74e8e3835378461
Royhan <royhankeren12@gmail.com>
2024-05-08 09:05:08 +0700

    add firebase in project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    // Add the Google services Gradle plugin
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -66,4 +68,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    //noinspection UseTomlInstead
+    implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
+    //noinspection UseTomlInstead
+    implementation("com.google.firebase:firebase-analytics")
 }
