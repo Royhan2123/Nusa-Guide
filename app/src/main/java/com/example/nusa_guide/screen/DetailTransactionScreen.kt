715dcb3abe018ae0eca2c87b5155880ca4c3a7c3
Oppie Ramadhanti <oppieramadhanti@gmail.com>
2024-05-28 22:49:54 +0700

    feat: add Transaction Success Screen

--- app/src/main/java/com/example/nusa_guide/screen/DetailTransactionScreen.kt
+++ app/src/main/java/com/example/nusa_guide/screen/DetailTransactionScreen.kt
@@ -11,14 +11,18 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -63,6 +67,17 @@ fun DetailTransactionScreen() {
             Spacer(modifier = Modifier.height(10.dp))
             Text("Transfer Bank BCA", fontSize = 18.sp, color = Gray60)
         }
+        Spacer(modifier = Modifier.height(30.dp))
+
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = "Jelajahi Nusa Guide",
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Black
+
+        )
+
     }
 }
 
